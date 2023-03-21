ec885701863c85ca336021feff905c77a5b75747
vikuli <vikukulichka.terekhova@gmail.com>
2023-03-21 12:41:02 +0300

    docs: create footer component

--- concept-store/src/app/app.module.ts
+++ concept-store/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
--- concept-store/src/app/shared/shared.module.ts
+++ concept-store/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, FormsModule],
   exports: [HeaderComponent],
 })
