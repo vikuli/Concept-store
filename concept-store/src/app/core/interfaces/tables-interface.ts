59f1a19ced5812c05655bd63a70e285a25ca0841
vikuli <vikukulichka.terekhova@gmail.com>
2023-03-23 14:30:52 +0300

    feat: add data about products from the category Tables

---
+++ concept-store/src/app/core/interfaces/tables-interface.ts
@@ -0,0 +1,14 @@
+export interface Tables {
+  category: string;
+  name: string;
+  fullName: string;
+  productInfo: string;
+  img: string;
+  price: number;
+  length: number;
+  width: number;
+  height: number;
+  type: string;
+  shape: string;
+  color: string | string[];
+}
