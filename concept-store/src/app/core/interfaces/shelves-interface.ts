8e78546c566090a55aad0299bceaf3fbf8a2b1c9
vikuli <vikukulichka.terekhova@gmail.com>
2023-04-05 12:42:36 +0300

    feat: add data about products from the category Shelves

---
+++ concept-store/src/app/core/interfaces/shelves-interface.ts
@@ -0,0 +1,14 @@
+export interface Shelves {
+  category: string;
+  name: string;
+  fullName: string;
+  productInfo: string;
+  img: string;
+  price: number;
+  length: number;
+  numberOfShelves: number;
+  type: string;
+  feature: string;
+  shape: string;
+  color: string[];
+}
