0b9007aac77de2f49b4b267000456ff78ff130a5
vikuli <vikukulichka.terekhova@gmail.com>
2023-04-06 14:21:35 +0300

    feat: add data about products from the category Lamps

---
+++ concept-store/src/app/core/interfaces/lamps-interface.ts
@@ -0,0 +1,13 @@
+export interface Lamps {
+  category: string;
+  name: string;
+  fullName: string;
+  productInfo: string;
+  img: string;
+  price: number;
+  length: number;
+  switchType: string[];
+  type: string;
+  feature: string[];
+  color: string[];
+}
