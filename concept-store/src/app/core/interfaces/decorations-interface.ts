4e1375dbab0ecc44f8761139ae6ec7b144f4cfdd
vikuli <vikukulichka.terekhova@gmail.com>
2023-04-07 11:23:12 +0300

    feat: add data about products from the category Decorations

---
+++ concept-store/src/app/core/interfaces/decorations-interface.ts
@@ -0,0 +1,11 @@
+export interface Decorations {
+  category: string;
+  name: string;
+  fullName: string;
+  productInfo: string;
+  img: string;
+  price: number;
+  type: string;
+  material: string[];
+  color: string[];
+}
